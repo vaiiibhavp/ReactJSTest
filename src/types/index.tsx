d1445a39c425ed7027355d2d70fb16d592498401
Pinal Tilva <pinal@7span.com>
2023-01-23 11:54:33 +0530

    Initial commit

---
+++ src/types/index.tsx
@@ -0,0 +1,14 @@
+export interface dataObject {
+  Make: string;
+  Model: string;
+  "Vehicle Class": string;
+  "Engine Size(L)": number;
+  Cylinders: number;
+  Transmission: string;
+  "Fuel Type": string;
+  "Fuel Consumption City (L/100 km)": number;
+  "Fuel Consumption Hwy (L/100 km)": number;
+  "Fuel Consumption Comb (L/100 km)": number;
+  "Fuel Consumption Comb (mpg)": number;
+  "CO2 Emissions(g/km)": number;
+}
